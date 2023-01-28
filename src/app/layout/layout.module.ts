83a6bc5eab60e7789da548332ae7a630dea96e9a
lukb <lukas.buderath@gmail.com>
2023-01-28 12:00:44 +0100

    Implement a very simple spinner for products page load

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HorizontalMenuComponent } from './horizontal-menu/horizontal-menu.component';
 import { RouterModule } from '@angular/router';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
-  declarations: [HorizontalMenuComponent],
-  exports: [HorizontalMenuComponent],
+  declarations: [HorizontalMenuComponent, SpinnerComponent],
+  exports: [HorizontalMenuComponent, SpinnerComponent],
   imports: [CommonModule, RouterModule]
 })
 export class LayoutModule {}
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductViewComponent } from './product-view/product-view.component';
 import { ProductRoutingModule } from './product-routing.module';
+import { LayoutModule } from '../layout/layout.module';
 
 @NgModule({
   declarations: [ProductCardComponent, ProductViewComponent],
   exports: [ProductCardComponent, ProductViewComponent],
-  imports: [CommonModule, ProductRoutingModule]
+  imports: [CommonModule, ProductRoutingModule, LayoutModule]
 })
 export class ProductModule {}
